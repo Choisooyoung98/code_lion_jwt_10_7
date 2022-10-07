ecc025599581332ee4c5ae0a64db37eb7e25885a
Choisooyoung98 <ekfms15815@gmail.com>
2022-10-07 16:21:35 +0900

    세팅

---
+++ src/main/java/com/ll/exam/jwt_10_07/Jwt1007Application.java
@@ -0,0 +1,13 @@
+package com.ll.exam.jwt_10_07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwt1007Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jwt1007Application.class, args);
+	}
+
+}
